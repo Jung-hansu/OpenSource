0ef3b9955b8c423c72c20c21e4e43ab4d3ee24fe
정한수 <jounghs3933@gmail.com>
2023-12-16 21:19:59 +0900

    secret_key 로컬저장

--- soloProject/settings.py
+++ soloProject/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_c5k-eon=_m-mqi_oi(i)xbu$b2vdo_92@wx_1k+$ag20zb60-'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
